566e8e532ed70f4c1121a5d9c480a4ffa6519139
AINUR NAJIEHA BINTI HUZWALI <2025159787@student.uitm.edu.my>
2026-01-18 05:40:23 +0800

    configure cloudinary bean

--- src/main/java/com/petboss/config/CloudinaryConfig.java
+++ src/main/java/com/petboss/config/CloudinaryConfig.java
@@ -1,4 +1,23 @@
 package com.petboss.config;
 
+import com.cloudinary.Cloudinary;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ApplicationScoped
 public class CloudinaryConfig {
+
+    @Produces
+    public Cloudinary cloudinary() {
+
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", "dj1p3z9ko");
+        config.put("api_key", "732371368313435");
+        config.put("api_secret", "H2thykIMnyUEi4cvOJ-fRYAfXhE");
+
+        return new Cloudinary(config);
+    }
 }
